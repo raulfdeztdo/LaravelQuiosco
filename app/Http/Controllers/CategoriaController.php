e538836e1f09da1320645821cb27e639bdd0b079
Raúl Fernández Tirado <raulfdeztdo@gmail.com>
2024-05-07 19:34:12 +0200

    Se instala extensión de api para Laravel para instalar ficheros y librería Sanctum y que nuestro proyecto pueda ser una API, añadimos endopoint publico temporalmente para probar CategoriaController

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
-    //
+    public function index()
+    {
+        return Categoria::all();
+    }
 }
