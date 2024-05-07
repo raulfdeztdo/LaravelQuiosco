8fe03f0b438f680ab0001953aa6cec8a75167b28
Raúl Fernández Tirado <raulfdeztdo@gmail.com>
2024-05-07 19:37:27 +0200

    Creamos collection y resource para el modelo Categoria y modificamos el resource para solo devolver los campos id, nombre e icono

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Categoria;
 use Illuminate\Http\Request;
+use App\Http\Resources\CategoriaCollection;
 
 class CategoriaController extends Controller
 {
     public function index()
     {
-        return Categoria::all();
+        return new CategoriaCollection(Categoria::all());
     }
 }
