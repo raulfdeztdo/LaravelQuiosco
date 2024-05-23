7b01da1e5d847b46846972aa6aed462f9a50d2b8
Raúl Fernández Tirado <raulfdeztdo@gmail.com>
2024-05-23 20:06:12 +0200

    Se añade toda la configuración de producto: modelo, controlador, resource, collection, migración, seeder y se añade endpoint para dar todos los productos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ProductoSeeder;
 use Database\Seeders\CategoriaSeeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,5 +16,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(CategoriaSeeder::class);
+        $this->call(ProductoSeeder::class);
     }
 }
